e7ae865f13fa7a192e247b3866ffadfe54061bd9
sanjay-au <sanjayguptha515@gmail.com>
2024-06-30 18:48:45 +0530

    updated

--- realestate/realestate/spiders/agents.py
+++ realestate/realestate/spiders/agents.py
@@ -6,27 +6,33 @@ class AgentSpider(scrapy.Spider):
     start_urls=['https://www.bhhsamb.com/bio/Agents']
 
     def parse(self, response):
-        for ag in response.css('a.cms-int-roster-card-image-container.site-roster-card-image-link').attrib['href']:
-            details=response.css('section.rng-agent-profile.bio1')
-            yield {
-                'name':'',
-                'job_title':'',
-                'image_url':'',
-                'address':'',
-                'contact_details':{
-                    'Office':'',
-                    'Cell':'',
-                    'Fax':'',
-                },
-                'social_accounts':{
-                    'facebook':'',
-                    'twitter':'',
-                    'linkedin':'',
-                    'youtube':'',
-                    'pinterest':'',
-                    'instagram':''
-                },
-                'offices':[],
-                'languages':'',
-                'description':''
-            }
+            profile_links=response.css('a.cms-int-roster-card-image-container::attr(href)').getall()
+            for links in profile_links:
+                yield response.follow(links,self.parse_details)
+
+    def parse_details(self,response):
+        profile=response.css('section.rng-agent-profile').get()
+        languages=profile.css('p.rng-agent-profile-languages::text').get()
+
+        yield {
+            'name':profile.css('p.rng-agent-profile-contact-name::text').get(),
+            'job_title':profile.css('span.rng-agent-profile-contact-title::text').get(),
+            'image_url':profile.css('img.rng-agent-profile-photo::attr(src)').get(),
+            'address':profile.css('li.rng-agent-profile-contact-address::text').get(),
+            'contact_details':{
+                'phone':profile.css('i.rni-profile::text').get(),
+                'website':profile.css('rng-agent-profile-contact-website.a::attr(href)').get(),
+                'email':profile.css('rng-agent-profile-contact-email.a::attr(href)').get(),
+            },
+            'social_accounts':{
+                'facebook':'',
+                'twitter':'',
+                'linkedin':'',
+                'youtube':'',
+                'pinterest':'',
+                'instagram':''
+            },
+            'offices':[],
+            'languages':profile.css('p.rng-agent-profile-languages::text').get(),
+            'description':''
+        }
